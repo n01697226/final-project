00513bcb7ae4e5bd026ce24a0baabf3aa5a66ad8
n01697226 <n01697226@humber.ca>
2025-08-12 15:45:08 -0400

    cors issue solved

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://final-project-psi-ten.vercel.app",
+    credentials: true,
+  })
+);
 
 // routes
 app.use("/api/auth", authRoutes);
